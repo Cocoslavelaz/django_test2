ca39ee3af07374d6c9784322512461d40a4ce9a1
Cocoslavelaz <131766557+Cocoslavelaz@users.noreply.github.com>
2024-06-01 19:37:16 +0800

    Update views.py

--- mysite/translate/views.py
+++ mysite/translate/views.py
@@ -5,6 +5,7 @@ from azure.ai.translation.text.models import InputTextItem
 
 # Create your views here.
 def index(request):
+    output = ""
     T_REGION = 'eastus' # 填入位置/區域
     T_KEY = '8172170dfee1419e96108bfd158af72e' # 填入金鑰
     T_ENDPOINT = 'https://api.cognitive.microsofttranslator.com/' # 填入文字翻譯的 Web API
